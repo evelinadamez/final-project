354cdbb6bd6a7746d77b7d96a897fc0611424368
Evelyn adames <evelinadamez662@gmail.com>
2022-04-22 19:29:16 -0400

    Se agrego la clase contacto

---
+++ final-project/Contacto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace final_project
+{
+    public class Contacto
+    {
+        public int id { get; set; }
+        public string nombre { get; set; }
+        public string apellido { get; set; }
+        public string direccion { get; set; }
+        public string telefono { get; set; }
+        public string email { get; set; }
+    }
+}
